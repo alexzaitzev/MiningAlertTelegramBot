898e304fb4f636450aca1cff1e521aec2c96b92f
alexzaitzev <mail@alexzaitzev.pro>
2017-10-31 00:10:03 +0300

    Add event to stop polling server instead of cycle with time.sleep

--- commands.py
+++ commands.py
@@ -5,18 +5,18 @@ import json
 import logging
 import requests
 import telebot
-import time
 import threading
 
 monitoring_enabled = False
 
 bot = telebot.TeleBot(config.token)
 
+cancelEvent = threading.Event()
 
-def task1():
-    while monitoring_enabled:
+
+def poll_server(stop_event):
+    while not stop_event.wait(config.interval_polling):
         get_workers()
-        time.sleep(config.interval_polling)
     pass
 
 
@@ -31,7 +31,7 @@ def begin(message):
         bot_answer = "Monitoring has been started"
         logging.info("%s by %s" % (bot_answer, message.chat.username))
         bot.send_message(message.chat.id, bot_answer)
-        t1 = threading.Thread(target=task1)
+        t1 = threading.Thread(target=poll_server, args=(cancelEvent,))
         t1.start()
         t1.join()
 
@@ -43,6 +43,7 @@ def end(message):
         bot_answer = "Monitoring has already been stopped"
         bot.send_message(message.chat.id, bot_answer)
     else:
+        cancelEvent.set()
         bot_answer = "Monitoring has been stopped"
         bot.send_message(message.chat.id, bot_answer)
         logging.info("%s by %s" % (bot_answer, message.chat.username))
@@ -50,16 +51,23 @@ def end(message):
 
 
 def get_workers():
-    while monitoring_enabled:
-        response = requests.get(config.link)
-        json_response = json.loads(response.text)
-        workers_array = json_response['result']['workers']
+    prev_workers = set()
+    response = requests.get(config.link)
+    json_response = json.loads(response.text)
+    workers_array = json_response['result']['workers']
 
-        unique_workers = set()
-        for worker in workers_array:
-            unique_workers.add(worker[0])
+    unique_workers = set()
+    for worker in workers_array:
+        unique_workers.add(worker[0])
 
-        print("запрос")
-        xor = config.all_workers ^ unique_workers
-        if xor != set():
-            bot.send_message(config.channel_id, "\U000026A0 Есть неработающие воркеры \U000026A0 \n" + '\n'.join(xor))
+    workers_off = config.all_workers.difference(unique_workers)
+    if workers_off == set():
+        prev_workers.clear()
+    else:
+        if prev_workers == set():
+            prev_workers = prev_workers.union(workers_off)
+            logging.warning("The following workers are absent: %s", ', '.join(prev_workers))
+        else:
+            workers_down = workers_off.intersection(prev_workers)
+            if workers_down != set():
+                bot.send_message(config.channel_id, "\U000026A0 Absent workers: \U000026A0 \n" + '\n'.join(workers_off))
